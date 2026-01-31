70f5e4c406486d86e708aca46870bdcc09c576f3
Rac00nCanFly <aleksandra.dziurok@gmail.com>
2026-01-31 20:05:07 +0100

    Added docstrings and optimized functions v2

--- scripts/beat_detector.py
+++ scripts/beat_detector.py
@@ -1,15 +1,30 @@
 import librosa
 import numpy as np
 import matplotlib.pyplot as plt
+
 def load_audio(filepath):
     y, sr = librosa.load(filepath)
-
     return y,sr
 
 def detect_beats(y, sr):
-    onsets = librosa.onset.onset_detect(y, sr=sr)
-    beat_times = librosa.frames_to_time(onsets, sr=sr)
-    return beat_times
+    """Detects beats in an audio file
+    Args:
+        y: audio-time series np.ndarray [shape=(…, n)]
+        sr: sampling rate of y
 
-def plot_beats(y, sr, beat_times):
-    return librosa.display.specshow()
+    Returns:
+        np.ndarray [shape=(n_onsets,) or onset_envelope.shape]
+        estimated positions of detected onsets in time series"""
+    onsets = librosa.onset.onset_detect(y=y,sr=sr, units = 'time')
+    #beat_times = librosa.frames_to_time(onsets, sr=sr)
+    return onsets
+
+def plot_beats(y, sr):
+    return librosa.display.waveshow(y, sr=sr)
+
+y, sr = load_audio("audio_samples/Metronome 120 BPM - QuickSounds.com.mp3")
+print(y,sr)
+detected_beats = detect_beats(y, sr)
+print(detected_beats)
+plot = plot_beats(y, sr)
+plt.savefig("beat_detector.png")
